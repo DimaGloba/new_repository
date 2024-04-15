9b86efb5644b2cecf788ea2314b81d79d25f5626
DimaGloba <94780134+DimaGloba@users.noreply.github.com>
2024-04-15 16:26:50 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+import json
+
+from flask import Flask, request, redirect, url_for, render_template#импорт библиотеки Flask
+
+app = Flask(__name__) #экземпляр класса Flask с аргументом __name__
+
+users = {"Дмитрий": "123",
+         "Олег": "456",
+         "Михаил": "789"
+         }
+
+class Users:
+    def add_user(self, username, password):
+        try:
+            if username in users.keys():
+                return False
+            else:
+                users[username] = password
+                return True
+        except KeyError:
+            return False
+
+    def del_user(self):
+        pass
+
+    def get_user(self, username, password):
+        try:
+            if users[username] == password:
+                return "Пользователь с таким именем уже зарегестрирован"
+            else:
+                return "Пользователь успешно создан"
+        except KeyError:
+            return False
+
+user = Users()
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    username = ""
+    password = ""
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        if user.get_user(username, password):
+            return render_template('user_window.html', username=username)
+
+    return render_template('login.html')
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+@app.route('/regis', methods=['GET', 'POST'])
+def regis():
+    username = ""
+    password = ""
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        render_template('login.html')
+    return render_template('regis.html')
+
+
+print(users)
+
+if __name__ == "__main__":
+    app.run()
+    print(users)
